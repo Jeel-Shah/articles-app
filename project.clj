57d6c6056fae08865495c14abcb764f619584d4f
Jeel Shah <jeelshah96@gmail.com>
2017-05-07 16:42:47 -0400

    Added feedme for rss parsing, created read-rss function

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject articles-app "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple web app for me to get all of my favorite news"
+  :url "http://ohlogn.com"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [feedme "0.0.3"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler articles-app.handler/app}
   :profiles
