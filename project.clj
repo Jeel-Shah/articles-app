4a84dc5976c15e0c1da87e5bd86cef8b57d064b0
Jeel Shah <jeelshah96@gmail.com>
2017-05-07 17:21:22 -0400

    Added hiccup and sql deps

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
-                 [feedme "0.0.3"]]
+                 [feedme "0.0.3"]
+                 [hiccup "1.0.5"]
+                 [org.clojure/java.jdbc "0.7.0-alpha3"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler articles-app.handler/app}
   :profiles
