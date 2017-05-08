860b07994262aa6268ac98991e4cab82c7e61edb
Jeel Shah <jeelshah96@gmail.com>
2017-05-07 22:11:36 -0400

    Tangled handler.clj

--- src/articles_app/handler.clj
+++ src/articles_app/handler.clj
@@ -1,10 +1,18 @@
 (ns articles-app.handler
-  (:require [compojure.core :refer :all]
+  (:require [articles-app.retrieval-functions :as r-functions]
+            [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [hiccup.page]))
+
+(defn interesting-reads []
+  (hiccup.page/html5
+   {:lang "en"}
+   [:head [:title "Interesting Reads"]]
+   [:body (r-functions/pretty-top-10-reads (:entries (r-functions/read-rss "http://news.ycombinator.com/rss")))]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World!")
+  (GET "/" [] (interesting-reads))
   (route/not-found "Sorry, that doesn\'t make sense."))
 
 (def app
